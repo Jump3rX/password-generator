8d776feaf25dce004a9eaa03a4275dec686b412f
Jump3rX <33659768+Jump3rX@users.noreply.github.com>
2019-03-06 12:35:53 +0300

    Add files via upload

---
+++ password.py
@@ -0,0 +1,18 @@
+import time,random
+response = ["yes","okay","ok","sure"]
+characters = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*"
+print ("Do you want a secure password??")
+answer = input(">>")
+answer.lower()
+
+paslen = 8
+
+if answer in response:
+    print("let me make one for you...")
+    time.sleep(4)
+    print("Done!!")
+    time.sleep(2)
+    print ("" .join(random.sample(characters,paslen)))
+    time.sleep(6)
+else:
+    print("Hope you get hacked...bye!!")
